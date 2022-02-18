b714a101aa1e0488b7ef8f19bf1778b9f1ef321d
aniknia <aniknia.dev@gmail.com>
2022-02-18 02:13:53 -0800

    feat: return coordinates

--- flightmapper_back/routes.py
+++ flightmapper_back/routes.py
@@ -1,6 +1,10 @@
 from flightmapper_back import app
+from flightmapper_module import flightmapper
+
+flight_app = flightmapper.Action()
 
 # TODO: add version control
 @app.get("/v1/{route_start}/{route_end}")
 def get_routes(route_start: str, route_end: str):
-    return {"x_cords": [], "y_cords": []}
+    route = flight_app.main([[route_start, route_end]])
+    return {"x_cords": route["x_cords_0"], "y_cords": route["y_cords_0"]}
