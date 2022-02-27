5c1949bf4efef9419bad32c5dd579c14e567decd
aniknia <aniknia.dev@gmail.com>
2022-02-26 22:41:31 -0800

    feat: add route to check validity of airport code

--- flightmapper_back/routes.py
+++ flightmapper_back/routes.py
@@ -4,7 +4,20 @@ from flightmapper_module import flightmapper
 flight_app = flightmapper.Action()
 
 # TODO: add version control
+@app.get("/v1/")
+def default():
+    return {"default": True}
+
+
 @app.get("/v1/{route_start}/{route_end}")
 def get_routes(route_start: str, route_end: str):
     route = flight_app.main([[route_start, route_end]])
     return {"x_cords": route["x_cords_0"], "y_cords": route["y_cords_0"]}
+
+
+@app.get("/v1/{check_code}")
+def check_code(check_code: str):
+    if flight_app.airport_search(check_code) != False:
+        return {"check_code": True}
+    else:
+        return {"check_code": False}
