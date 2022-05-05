7808d2d0d287faa5aa22a72fb8dba9c89b96b917
aniknia <aniknia.dev@gmail.com>
2022-05-04 21:30:39 -0700

    feat: add skeleton of routes for time, distance, and carbon footprint

--- flightmapper_back/routes.py
+++ flightmapper_back/routes.py
@@ -14,6 +14,14 @@ def default():
 # TODO: add error handling
 
 
+@app.get("/v1/check-code/{check_code}")
+def check_code(check_code: str):
+    if flight_app.airport_search(check_code) != False:
+        return {"check_code": True}
+    else:
+        return {"check_code": False}
+
+
 @app.get("/v1/get-route/{route_start}/{route_end}")
 def get_route(route_start: str, route_end: str):
     route = flight_app.main([[route_start, route_end]])
@@ -24,9 +32,25 @@ def get_route(route_start: str, route_end: str):
     }
 
 
-@app.get("/v1/check-code/{check_code}")
-def check_code(check_code: str):
-    if flight_app.airport_search(check_code) != False:
-        return {"check_code": True}
-    else:
-        return {"check_code": False}
+@app.get("/v1/get-time/{route_start}/{route_end}")
+def get_time(route_start: str, route_end: str):
+    return {
+        "error": False,
+        "time": 0,
+    }
+
+
+@app.get("/v1/get-distance/{route_start}/{route_end}")
+def get_distance(route_start: str, route_end: str):
+    return {
+        "error": False,
+        "distance": 0,
+    }
+
+
+@app.get("/v1/get-carbon/{route_start}/{route_end}")
+def get_carbon(route_start: str, route_end: str):
+    return {
+        "error": False,
+        "carbon": 0,
+    }
